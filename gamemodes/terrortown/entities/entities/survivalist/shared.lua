4a14cd5e19e3c4c10c5950d8f6f72d4fd0a37212
Alf21 <4lf-mueller@gmx.de>
2018-05-24 23:32:45 +0200

    Support for Rolesetup

--- gamemodes/terrortown/entities/entities/survivalist/shared.lua
+++ gamemodes/terrortown/entities/entities/survivalist/shared.lua
@@ -29,44 +29,61 @@ AddCustomRole("SURVIVALIST", { -- first param is access for ROLES array => ROLES
     togglable = true -- option to toggle a role for a client if possible (F1 menu)
 })
 
--- if sync of roles has finished
-hook.Add("TTT2_FinishedSync", "SurvInitT", function(ply, first)
-	if CLIENT and first then -- just on client and first init !
+if CLIENT then
+	-- if sync of roles has finished
+	hook.Add("TTT2_FinishedSync", "SurvInitT", function(ply, first)
+		if first then -- just on client and first init !
 
-		-- setup here is not necessary but if you want to access the role data, you need to start here
-		-- setup basic translation !
-		LANG.AddToLanguage("English", ROLES.SURVIVALIST.name, "Survivalist")
-		LANG.AddToLanguage("English", "info_popup_" .. ROLES.SURVIVALIST.name, 
-            [[You are a Survivalist! 
-            Try to survive and protect your mates if possible!]])
-		LANG.AddToLanguage("English", "body_found_" .. ROLES.SURVIVALIST.abbr, "This was a Survivalist...")
-		LANG.AddToLanguage("English", "search_role_" .. ROLES.SURVIVALIST.abbr, "This person was a Survivalist!")
-		LANG.AddToLanguage("English", "target_" .. ROLES.SURVIVALIST.name, "Survivalist")
-        LANG.AddToLanguage("English", "ttt2_desc_" .. ROLES.SURVIVALIST.name, [[The Survivalist is a better innocent, because he is able to access his own ([C]) shop.
-Try to protect the innocents!]])
-	    
-	    -- optional for toggling whether player can avoid the role
-		LANG.AddToLanguage("English", "set_avoid_" .. ROLES.SURVIVALIST.abbr, "Avoid being selected as Survivalist!")
-		LANG.AddToLanguage("English", "set_avoid_" .. ROLES.SURVIVALIST.abbr .. "_tip", 
-	        [[Enable this to ask the server not to select you as Survivalist if possible. Does not mean you are Traitor more often.]])
-	    
-	    ---------------------------------
+			-- setup here is not necessary but if you want to access the role data, you need to start here
+			-- setup basic translation !
+			LANG.AddToLanguage("English", ROLES.SURVIVALIST.name, "Survivalist")
+			LANG.AddToLanguage("English", "info_popup_" .. ROLES.SURVIVALIST.name, 
+				[[You are a Survivalist! 
+				Try to survive and protect your mates if possible!]])
+			LANG.AddToLanguage("English", "body_found_" .. ROLES.SURVIVALIST.abbr, "This was a Survivalist...")
+			LANG.AddToLanguage("English", "search_role_" .. ROLES.SURVIVALIST.abbr, "This person was a Survivalist!")
+			LANG.AddToLanguage("English", "target_" .. ROLES.SURVIVALIST.name, "Survivalist")
+			LANG.AddToLanguage("English", "ttt2_desc_" .. ROLES.SURVIVALIST.name, [[The Survivalist is a better innocent, because he is able to access his own ([C]) shop.
+	Try to protect the innocents!]])
+			
+			-- optional for toggling whether player can avoid the role
+			LANG.AddToLanguage("English", "set_avoid_" .. ROLES.SURVIVALIST.abbr, "Avoid being selected as Survivalist!")
+			LANG.AddToLanguage("English", "set_avoid_" .. ROLES.SURVIVALIST.abbr .. "_tip", 
+				[[Enable this to ask the server not to select you as Survivalist if possible. Does not mean you are Traitor more often.]])
+			
+			---------------------------------
 
-		-- maybe this language as well...
-		LANG.AddToLanguage("Deutsch", ROLES.SURVIVALIST.name, "Überlebenskünstler")
-		LANG.AddToLanguage("Deutsch", "info_popup_" .. ROLES.SURVIVALIST.name, 
-            [[Du bist ein Überlebenskünstler! 
-            Versuche zu überleben und beschütze dein Team, wenn es möglich sein sollte!]])
-		LANG.AddToLanguage("Deutsch", "body_found_" .. ROLES.SURVIVALIST.abbr, "Er war ein Überlebenskünstler...")
-		LANG.AddToLanguage("Deutsch", "search_role_" .. ROLES.SURVIVALIST.abbr, "Diese Person war ein Überlebenskünstler!")
-		LANG.AddToLanguage("Deutsch", "target_" .. ROLES.SURVIVALIST.name, "Überlebenskünstler")
-        LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. ROLES.SURVIVALIST.name, [[Der Überlebenskünstler ist ein besserer Unschuldiger (oder sogar ein besserer Detektiv), denn er hat seinen eigenen ([C]) Shop.
-Versuche, die Unschuldigen zu beschützen!]])
-	    
-		LANG.AddToLanguage("Deutsch", "set_avoid_" .. ROLES.SURVIVALIST.abbr, "Vermeide als Überlebenskünstler ausgewählt zu werden!")
-		LANG.AddToLanguage("Deutsch", "set_avoid_" .. ROLES.SURVIVALIST.abbr .. "_tip", 
-	        [[Aktivieren, um beim Server anzufragen, nicht als Überlebenskünstler ausgewählt zu werden. Das bedeuted nicht, dass du öfter Traitor wirst!]])
-	end
-end)
+			-- maybe this language as well...
+			LANG.AddToLanguage("Deutsch", ROLES.SURVIVALIST.name, "Überlebenskünstler")
+			LANG.AddToLanguage("Deutsch", "info_popup_" .. ROLES.SURVIVALIST.name, 
+				[[Du bist ein Überlebenskünstler! 
+				Versuche zu überleben und beschütze dein Team, wenn es möglich sein sollte!]])
+			LANG.AddToLanguage("Deutsch", "body_found_" .. ROLES.SURVIVALIST.abbr, "Er war ein Überlebenskünstler...")
+			LANG.AddToLanguage("Deutsch", "search_role_" .. ROLES.SURVIVALIST.abbr, "Diese Person war ein Überlebenskünstler!")
+			LANG.AddToLanguage("Deutsch", "target_" .. ROLES.SURVIVALIST.name, "Überlebenskünstler")
+			LANG.AddToLanguage("Deutsch", "ttt2_desc_" .. ROLES.SURVIVALIST.name, [[Der Überlebenskünstler ist ein besserer Unschuldiger (oder sogar ein besserer Detektiv), denn er hat seinen eigenen ([C]) Shop.
+	Versuche, die Unschuldigen zu beschützen!]])
+			
+			LANG.AddToLanguage("Deutsch", "set_avoid_" .. ROLES.SURVIVALIST.abbr, "Vermeide als Überlebenskünstler ausgewählt zu werden!")
+			LANG.AddToLanguage("Deutsch", "set_avoid_" .. ROLES.SURVIVALIST.abbr .. "_tip", 
+				[[Aktivieren, um beim Server anzufragen, nicht als Überlebenskünstler ausgewählt zu werden. Das bedeuted nicht, dass du öfter Traitor wirst!]])
+		end
+	end)
+	
+	-- modify roles table of rolesetup addon
+	hook.Add("TTTAModifyRolesTable", "ModifyRoleSurvToInno", function(rolesTable)
+		local tmp = {}
+		
+		for _, v in pairs(ROLES) do
+			tmp[v.index] = {}
+		end
+		
+		for role, ply in pairs(rolesTable) do
+			table.insert(tmp[(role == ROLES.SURVIVALIST.index and ROLES.INNOCENT.index or role)], ply)
+		end
+		
+		rolesTable = tmp
+	end)
+end
 
 -- nothing special, just a inno that is able to access the [C] shop
