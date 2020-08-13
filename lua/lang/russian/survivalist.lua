256949ef5bd6254e9345f72b2d5cf7e5a3c33d8e
Aram <dragolino6@mail.ru>
2020-08-14 00:40:28 +0300

    Added Russian localization

---
+++ lua/lang/russian/survivalist.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("russian")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SURVIVALIST.name] = "Выживальщик"
+L["info_popup_" .. SURVIVALIST.name] = [[Вы выживальщик!
+Постарайтесь выжить и по возможности защитить своих товарищей!]]
+L["body_found_" .. SURVIVALIST.abbr] = "Он был выживальщиком."
+L["search_role_" .. SURVIVALIST.abbr] = "Этот человек был выживальщиком!"
+L["target_" .. SURVIVALIST.name] = "Выживальщик"
+L["ttt2_desc_" .. SURVIVALIST.name] = [[Выживальщик — это улучшенный невиновный, поскольку у него есть доступ к собственному меню снаряжения ([C]).
+Постарайтесь защитить других невиновных!]]
