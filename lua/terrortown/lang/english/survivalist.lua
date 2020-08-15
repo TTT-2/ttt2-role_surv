fbdc9250762f687adf854310caec7fb54900537c
Tim Goll <tim@timgoll.de>
2020-08-15 18:03:42 +0200

    switched to new folder structure

--- lua/terrortown/lang/english/survivalist.lua
+++ lua/terrortown/lang/english/survivalist.lua
@@ -1,4 +1,4 @@
-L = LANG.GetLanguageTableReference("english")
+local L = LANG.GetLanguageTableReference("english")
 
 -- GENERAL ROLE LANGUAGE STRINGS
 L[SURVIVALIST.name] = "Survivalist"
@@ -7,5 +7,5 @@ Try to survive and protect your mates if possible!]]
 L["body_found_" .. SURVIVALIST.abbr] = "They were a Survivalist."
 L["search_role_" .. SURVIVALIST.abbr] = "This person was a Survivalist!"
 L["target_" .. SURVIVALIST.name] = "Survivalist"
-L["ttt2_desc_" .. SURVIVALIST.name] = [[The Survivalist is a better innocent, because he is able to access his own ([C]) shop.
+L["ttt2_desc_" .. SURVIVALIST.name] = [[The Survivalist is a better innocent, because they are able to access their own ([C]) shop.
 Try to protect the innocents!]]
