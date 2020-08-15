04ad8b8da0cd6867a10a6a56c6a64af88def473a
Tekiad <tekiadp@gmail.cm>
2020-08-15 13:02:13 -0300

    Create survivalist.lua (#2)

---
+++ lua/lang/spanish/survivalist.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("Español")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SURVIVALIST.name] = "Superviviente"
+L["info_popup_" .. SURVIVALIST.name] = [[¡Eres un Superviviente!
+¡Intenta sobrevivir con tu única herramienta!]]
+L["body_found_" .. SURVIVALIST.abbr] = "¡Era un Superviviente!"
+L["search_role_" .. SURVIVALIST.abbr] = "Esta persona era un Superviviente"
+L["target_" .. SURVIVALIST.name] = "Superviviente"
+L["ttt2_desc_" .. SURVIVALIST.name] = [[El Superviviente es un inocente con experiencia, lo que lo habilita a hacer uso de su perk de Superviviente, 
+la tienda([C]). Recuerda, tienes un sólo objeto de todas las opciones.]]
