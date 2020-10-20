d66f301a4b51a23e599914decdd06d1c4aaad0f9
Alessio <36534723+ThePlatinumGhost@users.noreply.github.com>
2020-10-20 21:53:59 +0200

    Update survivalist.lua (#4)

--- lua/terrortown/lang/italiano/survivalist.lua
+++ lua/terrortown/lang/italiano/survivalist.lua
@@ -1,11 +1,11 @@
 local L = LANG.GetLanguageTableReference("italiano")
 
 -- GENERAL ROLE LANGUAGE STRINGS
-L[SURVIVALIST.name] = "Survivalist"
-L["info_popup_" .. SURVIVALIST.name] = [[Sei un Survivalist!
+L[SURVIVALIST.name] = "Survivalista"
+L["info_popup_" .. SURVIVALIST.name] = [[Sei un Survivalista!
 Prova a sopravvivere e proteggere i tuoi compagni, se possibile!]]
-L["body_found_" .. SURVIVALIST.abbr] = "Era un Survivalist..."
-L["search_role_" .. SURVIVALIST.abbr] = "Questa persona era un Survivalist!"
-L["target_" .. SURVIVALIST.name] = "Survivalist"
-L["ttt2_desc_" .. SURVIVALIST.name] = [[Il Survivalist is è un innocente migliore, perché può accedere al suo shop con ([C]).
+L["body_found_" .. SURVIVALIST.abbr] = "Era un Survivalista..."
+L["search_role_" .. SURVIVALIST.abbr] = "Questa persona era un Survivalista!"
+L["target_" .. SURVIVALIST.name] = "Survivalista"
+L["ttt2_desc_" .. SURVIVALIST.name] = [[Il Survivalista è un innocente migliore, perché può accedere al suo shop con ([C]).
 Prova a proteggere gli innocenti!]]
