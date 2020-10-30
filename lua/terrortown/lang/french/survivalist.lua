d686f40fe2a4c493fc565287de322aef3a0ec1da
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-10-30 21:40:17 +0100

    Create survivalist.lua (#5)
    french translation :Survivalist

---
+++ lua/terrortown/lang/french/survivalist.lua
@@ -0,0 +1,11 @@
+local L = LANG.GetLanguageTableReference("french")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SURVIVALIST.name] = "Survivaliste"
+L["info_popup_" .. SURVIVALIST.name] = [[Vous êtes un Survivaliste!
+Essayez de survivre et de protéger vos compagnons si possible!]]
+L["body_found_" .. SURVIVALIST.abbr] = "C'était un Survivaliste."
+L["search_role_" .. SURVIVALIST.abbr] = "Cette personne était un Survivaliste!"
+L["target_" .. SURVIVALIST.name] = "Survivaliste"
+L["ttt2_desc_" .. SURVIVALIST.name] = [[Le Survivaliste est un meilleur innocent, car ils peuvent accéder à leur propre ([C]) shop.
+Essayer de protéger les innocents!]]
