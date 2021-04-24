19196e7c53c984ab8d6d05460af7089b7b5a03b9
Tim Goll <tim@timgoll.de>
2021-04-24 13:44:00 +0200

    updated score vars

--- lua/terrortown/entities/roles/survivalist/shared.lua
+++ lua/terrortown/entities/roles/survivalist/shared.lua
@@ -7,21 +7,20 @@ end
 function ROLE:PreInitialize()
 	self.color = Color(80, 140, 90, 255)
 
-	self.abbr = "surv" -- abbreviation
-	self.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
-	self.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
-	self.scoreTeamKillsMultiplier = -8 -- multiplier for teamkill
-	self.unknownTeam = true -- player don't know their teammates
+	self.abbr = "surv"
+	self.score.killsMultiplier = 2
+	self.score.teamKillsMultiplier = -8
+	self.unknownTeam = true
 
-	self.defaultTeam = TEAM_INNOCENT -- the team name: roles with same team name are working together
-	self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
+	self.defaultTeam = TEAM_INNOCENT
+	self.defaultEquipment = SPECIAL_EQUIPMENT
 
 	self.conVarData = {
-		pct = 0.15, -- necessary: percentage of getting this role selected (per player)
-		maximum = 2, -- maximum amount of roles in a round
-		minPlayers = 7, -- minimum amount of players until this role is able to get selected
-		credits = 1, -- the starting credits of a specific role
-		togglable = true, -- option to toggle a role for a client if possible (F1 menu)
+		pct = 0.15,
+		maximum = 2,
+		minPlayers = 7,
+		credits = 1,
+		togglable = true,
 		shopFallback = SHOP_FALLBACK_DETECTIVE
 	}
 end
