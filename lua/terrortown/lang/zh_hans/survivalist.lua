b52e4675124b98e8d09b3481d013f52b733d67a4
sbzl <92261479+sbzlzh@users.noreply.github.com>
2022-09-03 00:38:23 +0800

    Add simplified Chinese

---
+++ lua/terrortown/lang/zh_hans/survivalist.lua
@@ -0,0 +1,13 @@
+local L = LANG.GetLanguageTableReference("zh_hans")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[SURVIVALIST.name] = "黑商"
+L["info_popup_" .. SURVIVALIST.name] = [[你是个黑商!
+
+如果可能的话,试着生存并保护你的伴侣!]]
+L["body_found_" .. SURVIVALIST.abbr] = "他们是黑商."
+L["search_role_" .. SURVIVALIST.abbr] = "这个人是黑商!"
+L["target_" .. SURVIVALIST.name] = "黑商"
+L["ttt2_desc_" .. SURVIVALIST.name] = [[黑商是一个更好的无辜者,因为他们能够访问自己的([C])商店.
+
+努力保护无辜者!]]
